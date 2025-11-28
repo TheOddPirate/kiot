95bcdb7b0a875de7f95e8b129a3a610314556743
TheOddPirate <oddostlie@gmail.com>
2025-11-28 19:20:11 +0100

    tried to make the code more readable

--- integrations/bluetooth.cpp
+++ integrations/bluetooth.cpp
@@ -7,67 +7,80 @@
 #include <BluezQt/Manager>
 #include <BluezQt/Adapter>
 #include <BluezQt/InitManagerJob>
+#include <QDBusConnection>
+#include <QDBusInterface>
+#include <QDebug>
 
 class BluetoothAdapterWatcher : public QObject
 {
     Q_OBJECT
 
 public:
-    explicit BluetoothAdapterWatcher(QObject *parent = nullptr)
-        : QObject(parent)
-    {
-        m_switch = new Switch(this);
-        m_switch->setId("bluetooth_adapter");
-        m_switch->setName("Bluetooth Adapter");
-        m_manager = new BluezQt::Manager(this);
+    explicit BluetoothAdapterWatcher(QObject *parent = nullptr);
 
-        // Lag init-jobben
-        BluezQt::InitManagerJob *job = m_manager->init();
-        connect(m_switch, &Switch::stateChangeRequested, this, [this](bool requestedState){
-            auto adapters = m_manager->adapters(); // Hent powered adapter
-            if (!adapters.isEmpty()) {
-                auto adapter = adapters.first();
-            if (requestedState) {
-                adapter->setPowered(true);  // Skru på
-                qDebug() << "Turned on bluetooth adaper: " << adapter->name();
-            } else {
-                adapter->setPowered(false); // Skru av
-                qDebug() << "Turned off bluetooth adaper: " << adapter->name();
-            }
-            }
-        });
-        // Koble signal for når init er ferdig
-        connect(job, &BluezQt::InitManagerJob::result, this, [this, job]() {
-            if (job->error()) {
-                qWarning() << "Bluez init failed:" << job->errorText();
-                m_switch->setState(false);
-                return;
-            }
-
-auto adapters = job->manager()->adapters();
-if (!adapters.isEmpty()) {
-    auto adapter = adapters.first(); // Hent første adapter uansett powered state
-    qDebug() << "Adapter:" << adapter->name() << "Powered:" << adapter->isPowered();
-    m_switch->setState(adapter->isPowered());
-    
-    connect(adapter.data(), &BluezQt::Adapter::poweredChanged, this, [this](bool powered){
-        m_switch->setState(powered);
-    });
-} else {
-    qWarning() << "No adapters found at all";
-    m_switch->setState(false);
-}
-        });
-
-        job->start();
-    }
 
     
 private:
-    Switch *m_switch;
-    BluezQt::Manager *m_manager;
+
+    Switch *m_switch = nullptr;
+    BluezQt::Manager *m_manager = nullptr;
+    BluezQt::AdapterPtr m_adapter;
+    bool m_initialized = false;
 };
 
+BluetoothAdapterWatcher::BluetoothAdapterWatcher(QObject *parent)
+    : QObject(parent)
+{
+    m_switch = new Switch(this);
+    m_switch->setId("bluetooth_adapter");
+    m_switch->setName("Bluetooth Adapter");
+
+    m_manager = new BluezQt::Manager(this);
+
+    // Lag init-jobben
+    BluezQt::InitManagerJob *job = m_manager->init();
+
+    connect(job, &BluezQt::InitManagerJob::result, this, [this, job]() {
+        if (job->error()) {
+            qWarning() << "Bluez init failed:" << job->errorText();
+            m_switch->setState(false);
+            return;
+        }
+
+        auto adapters = job->manager()->adapters();
+        if (!adapters.isEmpty()) {
+            m_adapter = adapters.first(); // Første adapter
+            m_initialized = true;
+
+            qDebug() << "Adapter:" << m_adapter->name() << "Powered:" << m_adapter->isPowered();
+            m_switch->setState(m_adapter->isPowered());
+
+            connect(m_adapter.data(), &BluezQt::Adapter::poweredChanged, this, [this](bool powered){
+                m_switch->setState(powered);
+            });
+
+        } else {
+            qWarning() << "No adapters found";
+            m_switch->setState(false);
+        }
+    });
+
+    job->start();
+
+    // Koble til stateChangeRequested for HA
+    connect(m_switch, &Switch::stateChangeRequested, this, [this](bool requestedState){
+        if (!m_initialized || !m_adapter)
+            return;
+
+        m_adapter->setPowered(requestedState);
+        qDebug() << "Set adapter powered to" << requestedState;
+    });
+}
+
+
+
+
+// Setup-funksjon som registreres i Kiot
 void setupBluetoothAdapter()
 {
     new BluetoothAdapterWatcher(qApp);
