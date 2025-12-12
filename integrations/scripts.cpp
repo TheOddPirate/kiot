2f87f77d7c7d61a9ed8f51b24253c2bdadf2b954
TheOddPirate <oddostlie@gmail.com>
2025-12-12 12:59:43 +0100

    Upgraded scripts integration. this got a shared textbox for every script, it only gets created if one of the scripts got "{arg}" in the exec line, and will replace "{arg}" with the content of the textbox on excecution to allow input variables. Adds suport of custom incons on scripts by setting "icon=mdi:script-text" in the scripts config will default to "mdi:script-text" but everything you have available in HA should work

--- integrations/scripts.cpp
+++ integrations/scripts.cpp
@@ -3,18 +3,18 @@
 
 #include "core.h"
 #include "entities/entities.h"
-#include <KSharedConfig>
-#include <KConfigGroup>
 #include <QAction>
-#include <QDebug>
-#include <QCoreApplication>
 #include <KProcess>
 
+#include <KConfigGroup>
 void registerScripts()
 {
     qInfo() << "Loading scripts";
     auto scriptConfigToplevel = KSharedConfig::openConfig()->group("Scripts");
     const QStringList scriptIds = scriptConfigToplevel.groupList();
+    // TODO make sure this is the best way to support input variables
+    Textbox *textb = nullptr;
+
     for (const QString &scriptId : scriptIds) {
         auto scriptConfig = scriptConfigToplevel.group(scriptId);
         const QString name = scriptConfig.readEntry("Name", scriptId);
@@ -24,17 +24,36 @@ void registerScripts()
             qWarning() << "Could not find script Exec entry for" << scriptId;
             continue;
         }
-
+        //Creates a shared textbox for input variables to script only if user has defined {arg} in the Exec line
+        if (exec.contains("{arg}") && textb == nullptr) {
+            textb = new Textbox(qApp);
+            textb->setId("scripts_arguments");
+            textb->setName("arguments");
+            textb->setDiscoveryConfig("icon", "mdi:console");
+        }
         auto button = new Button(qApp);
         button->setId(scriptId);
         button->setName(name);
+        //Custom icon if set in config defaults to script icon
+        QString icon = scriptConfig.readEntry("icon","mdi:script-text");
+        button->setDiscoveryConfig("icon", icon);
         // Home assistant integration supports payloads, which we could expose as args
         // maybe via some substitution in the exec line
-        QObject::connect(button, &Button::triggered, qApp, [exec, scriptId]() {
+        // I tried to include some substitution and support for custom icons, it it okay?
+        QObject::connect(button, &Button::triggered, qApp, [exec, scriptId,textb]() {
             qInfo() << "Running script " << scriptId;
+            // TODO make this better
+            QString ex = exec;
+            if(exec.contains("{arg}")  && textb != nullptr)
+            {
+
+                qDebug() << "customizing exec \""<< ex << "\" to: \""  << ex.replace("{arg}",textb->state()) << "\"";
+                ex = ex.replace("{arg}",textb->state());
+                textb->setState(""); //Clears the textbox after use, should it be kept?
+            }
             // DAVE TODO flatpak escaping
             KProcess *p = new KProcess();
-            p->setShellCommand(exec);
+            p->setShellCommand(ex);
             p->startDetached();
             delete p;
         });
