e4e2b413dec9835613e14e06e241036a9bf81a92
Odd Østlie <oddostlie@gmail.com>
2026-09-21 23:34:37 +0200

    feat(plugins): Notifications ported to plugin

---
+++ Plugins/Notifications/notificationplugin.cpp
@@ -0,0 +1,88 @@
+#include "notificationplugin.h"
+#include <KNotification>
+#include <QCoreApplication>
+
+
+using KIOTShared::PlatformHelper;
+
+DEFINE_PLUGIN_LOGGER(plugin_logger,Notifications)
+
+NotificationPlugin::NotificationPlugin(QObject *parent)
+    : QObject(parent)
+{
+}
+
+QString NotificationPlugin::name() const
+{
+    return QString(PLUGIN_NAME).replace("\"", "");
+}
+
+QString NotificationPlugin::description() const
+{
+    return QString(PLUGIN_DESCRIPTION).replace("\"", "") + QString(" ") + QString(PLUGIN_DOMAIN).replace("\"", "");
+}
+QUrl NotificationPlugin::url() const
+{
+    return QUrl(QString(PLUGIN_DOMAIN).replace("\"", ""));
+}
+QVersionNumber NotificationPlugin::version() const
+{
+    QString version = QString(PLUGIN_VERSION).replace("\"", "");
+    return QVersionNumber::fromString(version);
+}
+
+bool NotificationPlugin::checkCompatibility()
+{
+    return true;
+}
+
+bool NotificationPlugin::startPlugin()
+{
+    if(m_notify)
+        stopPlugin();
+
+    m_notify = new Notify(this);
+    m_notify->setId("notifications");
+    m_notify->setName("Notifications");
+    connect(m_notify, &Notify::notificationReceived, this, &NotificationPlugin::notificationCallback);
+    qCInfo(plugin_logger) << name() << " plugin started successfully";
+    return true;
+}
+
+bool NotificationPlugin::stopPlugin()
+{
+    if(m_notify)
+    {
+        m_notify->deleteLater();
+        m_notify = nullptr;
+    }
+    qCInfo(plugin_logger) << name() << " plugin stopped";
+    return true;
+}
+
+
+void NotificationPlugin::notificationCallback(QByteArray message)
+    {
+        QString title = QString(PROJECT_NAME);
+        QJsonParseError err;
+        QJsonDocument doc = QJsonDocument::fromJson(message, &err);
+        if (err.error == QJsonParseError::NoError && doc.isObject()) {
+            QJsonObject obj = doc.object();
+
+            
+            //SO home assistant mqtt notify entity does not support title as default
+            //but just incase of a hacker, we do a extra check to set it if included
+            if(obj.contains("title"))
+                title = obj.value("title").toString();
+            const QString body = obj.value("message").toString();
+            KNotification::event(KNotification::Notification, title, body);
+ 
+        } else {
+            // Plain text path, should never happen but better safe than sorry
+            QString body = QString::fromUtf8(message);
+            KNotification::event(KNotification::Notification, title, body);
+
+        }
+
+    }
+#include "notificationplugin.moc"
---
+++ Plugins/Notifications/notificationplugin.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <QObject>
+#include <KIOTShared/kiotshared.h>
+using KIOTShared::Entities::Notify;
+using KIOTShared::Plugins::KIOTPluginInterface;
+class NotificationPlugin : public QObject, public KIOTShared::Plugins::KIOTPluginInterface {
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID KIOTPluginInterface_iid FILE "plugin.json")
+    Q_INTERFACES(KIOTShared::Plugins::KIOTPluginInterface)
+
+public:
+    NotificationPlugin(QObject *parent = nullptr);
+    ~NotificationPlugin() override = default;
+
+    QString name() const override;
+    QString description() const override;
+    QUrl url() const override;
+    QVersionNumber version() const override;
+    bool checkCompatibility() override;
+    
+    bool startPlugin() override;
+    bool stopPlugin() override;
+private slots:
+    void notificationCallback(QByteArray message);
+    
+private:
+    Notify *m_notify = nullptr;
+};
